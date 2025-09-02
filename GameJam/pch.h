9f5e437e9d682d4f9ac0b8b0c881b1c3a2008c14
TraceofLight <rickyjun96@gmail.com>
2025-09-02 20:39:41 +0900

    feature: pch
    pch 추가
    - 기타 프로젝트 내용 수정
    - utf-8 빌드

---
+++ GameJam/pch.h
@@ -0,0 +1,13 @@
+// D3D 사용에 필요한 라이브러리들을 링크합니다.
+#pragma comment(lib, "user32")
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "d3dcompiler")
+
+// D3D 사용에 필요한 헤더파일들을 포함합니다.
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "imGui/imgui_impl_win32.h"
