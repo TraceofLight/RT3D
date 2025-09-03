98ebc1e4e8c16a82d9fa5f48d81cb54b66473276
nansu0425 <nansu0425@gmail.com>
2025-09-03 21:46:08 +0900

    feat: UTriangle
    - 이등변 삼각형으로 변경
    - 핀볼과 충돌 구현

--- GameJam/Mesh/Public/UTriangle.h
+++ GameJam/Mesh/Public/UTriangle.h
@@ -10,7 +10,7 @@ public:
 	FVector3 Location; // Incenter (내접원 중심) 월드 좌표
 	float Base; // 밑변 길이 (b)
 	float Height; // 높이 (h)
-	float Radius; // 내접원 반지름 (r) = (b*h) / (b+h+sqrt(b^2+h^2))
+	float Radius; // 내접원 반지름 = (B * H) / ( B + 2 * sqrt( (B/2)^2 + H^2 ) )
 	float Rotation; // 라디안 (Incenter 기준 회전)
 	float RotationSpeed; // 초당 회전 속도 (라디안)
 
