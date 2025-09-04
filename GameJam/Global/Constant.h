bd424b911ec45c081ff9eb23d598a04b4ec0f4dd
TraceofLight <rickyjun96@gmail.com>
2025-09-04 12:04:44 +0900

    feat: uball property
    볼록 충돌체 추가를 위한 uball 멤버 복원
    - 스코어 변수 전역 상수 헤더로 위치 변경
    - 그 외 기타 리팩토링
    
    # Conflicts:
    #	GameJam/Global/Constant.h
    #	GameJam/Mesh/Public/UBall.h

--- GameJam/Global/Constant.h
+++ GameJam/Global/Constant.h
@@ -1,7 +1,13 @@
 #pragma once
 
+// Screen Size
 constexpr int SCREEN_HEIGHT = 1024;
 constexpr int SCREEN_WIDTH = 1024;
+
+// Score System
+// 1초마다 100점, 충돌 시 10점
+constexpr int TIME_SCORE_PER_SECOND = 100;
+constexpr int COLLISION_SCORE = 10;
 
 constexpr float OBSTACLE_RESTITUTION = 0.8f; // 장애물 탄성 계수
 constexpr float PAD_RESTITUTION = 1.2f;      // 패드 탄성 계수
